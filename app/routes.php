1519f735e1df81d962fc7fca57ceabe4cfb8daec
mkhairul <mkhairul@gmail.com>
2014-09-17 16:21:02 +0800

    janus REST API

--- app/routes.php
+++ app/routes.php
@@ -18,3 +18,51 @@ Route::get('/', function()
 {
 	return View::make('mainpage');
 });
+
+Route::match(array('GET'), '/janus/{session?}', function($session=''){
+
+	$request = Request::instance();
+	$content = $request->getContent();
+
+	$param = Input::all();
+	$param_str = '';
+	foreach($param as $key => $val){
+			$param_str .= $key . '=' . $val . '&';
+	}
+	$param_str = rtrim($param_str, '&');
+	//echo $param_str;
+	//return $session;
+
+	$url = "http://192.168.1.155:8088/janus/$session?$param_str";
+	$ch = curl_init();
+
+	//set the url
+	curl_setopt($ch,CURLOPT_URL, $url);
+	$result = curl_exec($ch);
+	//close connection
+	curl_close($ch);
+});
+
+Route::match(array('POST'), '/janus/{param?}/{handle?}', function($param='', $handle=''){
+	$url = 'http://192.168.1.155:8088/janus';
+	if($param)
+	{
+			$url = $url . "/$param";
+	}
+	if($handle)
+	{
+			$url = $url . "/$handle";
+	}
+	$request = Request::instance();
+	$content = $request->getContent();
+	$ch = curl_init();
+	//set the url, POST data
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $content);
+
+	//execute post
+	$result = curl_exec($ch);
+
+	//close connection
+	curl_close($ch);
+});
